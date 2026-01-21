4707a6892906039e6e8daeb6000e3adc5edf2272
= <mattshady1067@gmail.com>
2026-01-21 17:22:02 +0300

    Feat: Customize API landing page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Event Reservation API is running',
+        'version' => '1.0.0',
+        'documentation' => 'See README.md for endpoints',
+        'status' => 'healthy'
+    ]);
 });
